fb8da9bc6e6d3596cc3369e8636d33385f65892d
Your Name <syed.khaleelrockzz@gmail.com>
2023-05-09 15:00:22 +0530

    i updated the models on book

--- demo/Book/models.py
+++ demo/Book/models.py
@@ -5,3 +5,5 @@ class Book(models.Model):
     name=models.CharField(max_length=888, null=False,blank= False)
     Email=models.EmailField(max_length=888, null=False,blank= False)
     Phonenumber=models.CharField(max_length=888, null=False,blank= False)
+    Mownika=models.CharField(max_length=888, null=False,blank= False)
+
